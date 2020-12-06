c7284c3c49eaaa9524029be7d848857cf2682e07
googie94 <googieyu@gmail.com>
2020-12-06 18:00:39 +0900

    add

--- web/settings.py
+++ web/settings.py
@@ -132,5 +132,5 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
