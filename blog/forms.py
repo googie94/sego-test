955e7d7391a2d3371e5905b8605209268ab7801f
googie94 <googieyu@gmail.com>
2020-12-31 00:25:59 +0900

    add

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,21 @@
 from django import forms
-
+from django.forms import Textarea
 from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
+	author=forms.CharField(widget=forms.TextInput(attrs={'class': 'inputFull mb10',}))
+	title=forms.CharField(widget=forms.TextInput(attrs={'class': 'inputFull mb10',}))
+	text=forms.CharField(widget=forms.Textarea(attrs={'class': 'inputFull height250 mb10',}))
+    
+	class Meta:
+		model = Post
+		fields = ('author', 'title', 'text')
 
-    class Meta:
-        model = Post
-        fields = ('author', 'title', 'text',)
 
 
 class CommentForm(forms.ModelForm):
+	text=forms.CharField(widget=forms.Textarea(attrs={'class': 'inputFull height250 mb10',}))
 
-    class Meta:
-        model = Comment
-        fields = ('author', 'text',)
+	class Meta:
+		model = Comment
+		fields = ('text',)
