23ff1d61e31e12485aa16dbc4e2354570479959c
googie <googieyu@gmail.com>
2021-02-07 16:36:27 +0900

    testpy hotfeed emoji remove

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+#
+from urllib.request import urlopen, HTTPError, URLError
+from bs4 import BeautifulSoup
 
-# Create your tests here.
+def getTitle(url):
+	try:
+		html = urlopen(url)
+	except HTTPError as e:
+		return None
+	try:
+		bs = BeautifulSoup(html.read(), 'html.parser')
+		for sibling in bs.find('table', {'id': 'giftList'}).tr.next_siblings:
+			print(sibling)
+	except AttributeError as e:
+		return None
+	return title
+
+title = getTitle('http://www.pythonscraping.com/pages/page3.html')
+if title == None:
+	print('Title could not be found')
+else:
+	print(title)
