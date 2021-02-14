9e7268335728258841a57271633b9f4116b6b833
googie <googieyu@gmail.com>
2021-02-15 02:25:08 +0900

    fix

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,11 +3,14 @@ from rest_framework import serializers
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'text', 'created_date', 'author', 'approved_post']
+	class Meta:
+		model = Post
+		fields = ['id', 'title', 'text', 'created_date', 'author', 'approved_post', 'comments']
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['id', 'author', 'text', 'created_date', 'post_id', 'approved_comment']
+	posts = PostSerializer(many=True, read_only=True)
+	class Meta:
+		model = Comment
+		fields = ['id', 'author', 'text', 'created_date', 'post_id', 'approved_comment', 'posts']
+
+
