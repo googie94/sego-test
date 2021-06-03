0242d589e906d2c248a4a2c8fa0c89dcbd7cc232
googie <googieyu@gmail.com>
2021-06-03 12:35:06 +0900

    security add

--- web/settings.py
+++ web/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(p46oggljoq)im-m-!h13j$jr&(n+b!mo9a9)_7h2kg&bomo%g'
+SECRET_KEY = my_settings.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['letsego.site', 'www.letsego.site', 'localhost']
+# ALLOWED_HOSTS = ['letsego.site', 'www.letsego.site', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
