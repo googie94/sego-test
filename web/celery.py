7abc2438a821eaea15d98bd230ef8b3107f13128
googie94 <sportfolio94@gmail.com>
2021-06-17 17:43:36 +0900

    add

--- web/celery.py
+++ web/celery.py
@@ -21,11 +21,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-
-@app.task
-def naver_scraping():
-	scraping_start()
-
 @app.task(bind=True)
 def debug_task(self):
     print('Request : {0!r}'.format(self.request))
--- web/tasks.py
+++ web/tasks.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from .celery import app
+# 
+from scraping.fn import scraping_start
 
 @app.task
-def hello(name):
-    return name
+def naver_scarping():
+	scarping_start()
